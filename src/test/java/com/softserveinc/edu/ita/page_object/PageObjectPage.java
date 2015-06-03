da6c0b966fd4575283cc88f7e4c98a6130ac761f
olya-arabchuk <olya.arabchuk@gmail.com>
2015-06-03 17:56:57 +0300

    update PageObjectPage as abstract

--- src/test/java/com/softserveinc/edu/ita/page_object/PageObjectPage.java
+++ src/test/java/com/softserveinc/edu/ita/page_object/PageObjectPage.java
@@ -2,7 +2,7 @@ package com.softserveinc.edu.ita.page_object;
 
 import org.openqa.selenium.WebDriver;
 
-public class PageObjectPage {
+public abstract class PageObjectPage {
     protected WebDriver driver;
 
     public PageObjectPage(WebDriver driver){
