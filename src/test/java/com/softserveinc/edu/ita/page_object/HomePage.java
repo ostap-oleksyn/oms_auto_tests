f71de3d1d129a2425e5bdeb67536e301e226fe7f
olya-arabchuk <olya.arabchuk@gmail.com>
2015-06-03 15:31:45 +0300

    update page_object base

--- src/test/java/com/softserveinc/edu/ita/page_object/HomePage.java
+++ src/test/java/com/softserveinc/edu/ita/page_object/HomePage.java
@@ -2,8 +2,9 @@ package com.softserveinc.edu.ita.page_object;
 
 import org.openqa.selenium.WebDriver;
 
-public class HomePage {
+public class HomePage extends PageObjectPage{
 
     public HomePage(WebDriver driver) {
+        super(driver);
     }
 }
