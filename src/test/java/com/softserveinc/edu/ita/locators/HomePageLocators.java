78e9c3566a11f692b5270d5aba24c30149253ff4
rsolov <romansoloviy@gmail.com>
2015-06-04 10:12:11 +0300

    changed Logout button locator

--- src/test/java/com/softserveinc/edu/ita/locators/HomePageLocators.java
+++ src/test/java/com/softserveinc/edu/ita/locators/HomePageLocators.java
@@ -9,6 +9,6 @@ public final class HomePageLocators {
 
     public static final By LOGIN_USER_INPUT = By.xpath(".//*[@id='edit']//tr[1]/td[2]/input");
     public static final By LOGIN_PASSWORD_INPUT = By.xpath(".//*[@id='edit']//tr[2]/td[2]/input");
-    public static final By LOG_OUT_BUTTON = By.xpath(".//*[@id='nav']//a");
+    public static final By LOG_OUT_BUTTON = By.xpath("//img[@alt='logout']");
 
 }
