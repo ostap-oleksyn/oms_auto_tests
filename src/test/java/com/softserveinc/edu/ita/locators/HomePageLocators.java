1699289f3a26ffe6ed4c87cf625890a7bbb3e54c
rsolov <romansoloviy@gmail.com>
2015-06-04 10:01:59 +0300

    rename class with locators HomePage to HomePageLocators

---
+++ src/test/java/com/softserveinc/edu/ita/locators/HomePageLocators.java
@@ -0,0 +1,14 @@
+package com.softserveinc.edu.ita.locators;
+
+import org.openqa.selenium.By;
+
+public final class HomePageLocators {
+
+    private HomePageLocators() {
+    }
+
+    public static final By LOGIN_USER_INPUT = By.xpath(".//*[@id='edit']//tr[1]/td[2]/input");
+    public static final By LOGIN_PASSWORD_INPUT = By.xpath(".//*[@id='edit']//tr[2]/td[2]/input");
+    public static final By LOG_OUT_BUTTON = By.xpath(".//*[@id='nav']//a");
+
+}
