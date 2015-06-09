772249cf0a676142f2deed6a954922c14c40c9d9
igor <ihor.ilnytskyi@gmail.com>
2015-06-09 19:59:39 +0300

    Changed locator was added.

--- src/test/java/com/softserveinc/edu/ita/locators/OrderingPageLocators.java
+++ src/test/java/com/softserveinc/edu/ita/locators/OrderingPageLocators.java
@@ -9,7 +9,7 @@ public final class OrderingPageLocators {
     }
 
     public static final By CREATE_NEW_ORDER_LINK = By.xpath(".//*[@id='content']/a[contains(text(), 'Create new order')]");
-    public static final By QUANTITY_OF_ROWS = By.xpath(".//*[@id='content']/a[contains(text(), 'Create new order')]");
+    public static final By QUANTITY_OF_ROWS = By.xpath(".//*[@id='allFoundAndFiltered']");
     public static final By CLICK_NEXT = By.xpath(".//*[@name='nextPage']");
     public static final By CLICK_FIRST = By.xpath(".//*[@name='firstPage']");
 
