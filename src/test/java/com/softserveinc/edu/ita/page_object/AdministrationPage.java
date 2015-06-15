40ce4fa0814abd0059d71bd35a3c021a7631fa8d
rsolov <romansoloviy@gmail.com>
2015-06-15 09:45:34 +0300

    added random choose

--- src/test/java/com/softserveinc/edu/ita/page_object/AdministrationPage.java
+++ src/test/java/com/softserveinc/edu/ita/page_object/AdministrationPage.java
@@ -1,6 +1,7 @@
 package com.softserveinc.edu.ita.page_object;
 
 import com.softserveinc.edu.ita.locators.AdministrationPageLocators;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import java.util.Random;
@@ -14,7 +15,12 @@ public class AdministrationPage extends LogOutBase {
     // edit user: FirstName: Vitaliy, Login: antonov, Region: North
     // TODO make universal edition opportunity
     public EditUserPage clickEditUserLink() {
-        driver.findElement(AdministrationPageLocators.EDIT_USER_LINK).click();
+
+        Random random = new Random();
+        int row = random.nextInt(5);
+        final By EDIT_USER_LINK = By.xpath(".//*[@id='table']/tbody/tr[" + row + "]/td[6]/a[contains(text(), 'Edit')]");
+
+        driver.findElement(EDIT_USER_LINK).click();
         return new EditUserPage(driver);
     }
 }
