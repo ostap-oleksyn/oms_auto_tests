2f12a4ce50614c42e9a5abf7230b81f5a3c2e4ad
ihor <ihor.ilnytskyi@gmail.com>
2015-06-16 09:56:11 +0300

    Locators.

--- src/test/java/com/softserveinc/edu/ita/locators/AdministrationPageLocators.java
+++ src/test/java/com/softserveinc/edu/ita/locators/AdministrationPageLocators.java
@@ -7,14 +7,29 @@ public enum AdministrationPageLocators implements ILocator {
 
     CREATE_NEW_USER_LINK(
             "Create new user link",
-            By.xpath(".//*[@id='list']/a[contains(text(), 'Create New User')]"));
+            By.xpath(".//*[@id='list']/a[contains(text(), 'Create New User')]")),
+    QUANTITY_OF_TABLE_PAGES(
+            "Quantity of table pages",
+            By.xpath(".//*[@id='pageCount']")),
+    TABLE_ROWS(
+            "Table rows",
+            By.xpath(".//*[@id='table']//tr")),
+    ROW_CELLS(
+            "Row cells",
+            By.tagName("td")),
+    FIRST_BUTTON(
+            "First button",
+            By.xpath(".//*[@id='first']")),
+    NEXT_BUTTON(
+            "Next button",
+            By.xpath(".//input[@id='next']"));
+    public static final String TABLE_COLUMN = ".//*[@id='table']/thead/tr/th/a[contains(text(), '%s')]";
 
     AdministrationPageLocators(String name, By locator) {
         this.name = name;
         this.locator = locator;
     }
 
-<<<<<<< HEAD
     private String name;
     private By locator;
 
@@ -32,13 +47,4 @@ public enum AdministrationPageLocators implements ILocator {
     public By getBy() {
         return this.locator;
     }
-=======
-    public static final By CREATE_NEW_USER_LINK = By.xpath(".//*[@id='list']/a[contains(text(), 'Create New User')]");
-    public static final By QUANTITY_OF_TABLE_PAGES = By.xpath(".//*[@id='pageCount']");
-    public static final By TABLE_ROWS = By.xpath(".//*[@id='table']//tr");
-    public static final By ROW_CELLS = By.tagName("td");
-    public static final By FIRST_BUTTON = By.xpath(".//*[@id='first']");
-    public static final By NEXT_BUTTON = By.xpath(".//input[@id='next']");
-    public static final String TABLE_COLUMN = ".//*[@id='table']/thead/tr/th/a[contains(text(), '%s')]";
->>>>>>> IFAA-48-2
 }
