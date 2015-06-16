a7d55662e8567b56b030ddf454ea44cace1c2a14
ASI <asi-info@ukr.net>
2015-06-16 15:57:31 +0300

    Locators trasferred to enums

--- src/test/java/com/softserveinc/edu/ita/locators/AdministrationPageLocators.java
+++ src/test/java/com/softserveinc/edu/ita/locators/AdministrationPageLocators.java
@@ -7,11 +7,20 @@ public enum AdministrationPageLocators implements ILocator {
 
     CREATE_NEW_USER_LINK(
             "Create new user link",
-            By.xpath(".//*[@id='list']/a[contains(text(), 'Create New User')]"));
+            By.xpath(".//*[@id='list']/a[contains(text(), 'Create New User')]")),
 
-    public static final By USERS_TABLE = By.xpath(".//*[@id='table']");
-    public static final By USERS_TABLE_ROWS = By.xpath(".//tbody/tr");
-    public static final By LAST_BUTTON = By.xpath(".//*[@id='last']");
+    USERS_TABLE(
+            "Users table",
+            By.xpath(".//*[@id='table']")),
+
+    USERS_TABLE_ROWS(
+            "Users table rows",
+            By.xpath(".//tbody/tr")),
+
+    LAST_BUTTON(
+            "Last Button",
+            By.xpath(".//*[@id='last']"));
+
     public static final String DELETE_LINK = ".//*[@id='table']/tbody/tr[%s]/td[7]/a";
     public static final String LOGIN_CELL = ".//*[@id='table']/tbody/tr[%s]/td[3]";
 
--- src/test/java/com/softserveinc/edu/ita/locators/NewUserPageLocators.java
+++ src/test/java/com/softserveinc/edu/ita/locators/NewUserPageLocators.java
@@ -1,25 +1,89 @@
 package com.softserveinc.edu.ita.locators;
 
+import com.softserveinc.edu.ita.interfaces.ILocator;
 import org.openqa.selenium.By;
 
-import java.util.ArrayList;
-import java.util.List;
+public enum NewUserPageLocators implements ILocator {
 
-public class NewUserPageLocators {
+    LOGIN_NAME_INPUT(
+            "Login name input",
+            By.xpath(".//*[@id='login']")),
 
-    public static final By LOGIN_NAME_INPUT = By.xpath(".//*[@id='login']");
-    public static final By FIRST_NAME_INPUT = By.xpath(".//*[@id='firstName']");
-    public static final By LAST_NAME_INPUT = By.xpath(".//*[@id='lastName']");
-    public static final By PASSWORD_INPUT = By.xpath(".//*[@id='password']");
-    public static final By CONFIRM_PASSWORD_INPUT = By.xpath(".//*[@id='confirmPassword']");
-    public static final By EMAIL_INPUT = By.xpath(".//*[@id='email']");
-    public static final By REGION_SELECT = By.xpath(".//*[@id='regionID']");
+    FIRST_NAME_INPUT(
+            "First name input",
+            By.xpath(".//*[@id='firstName']")),
+
+    LAST_NAME_INPUT(
+            "Last name input",
+            By.xpath(".//*[@id='lastName']")),
+
+    PASSWORD_INPUT(
+            "Password input",
+            By.xpath(".//*[@id='password']")),
+
+    CONFIRM_PASSWORD_INPUT(
+            "Confirm password input",
+            By.xpath(".//*[@id='confirmPassword']")),
+
+    EMAIL_INPUT(
+            "Email input",
+            By.xpath(".//*[@id='email']")),
+
+    REGION_SELECT(
+            "Region select",
+            By.xpath(".//*[@id='regionID']")),
+
+    CREATE_BUTTON(
+            "Create button",
+            By.xpath(".//input[@type='submit'][@value='Create']")),
+
+    LOGIN_NAME_ERROR_LABEL(
+            "Login name error label",
+            By.xpath(".//*[@id='nameError']")),
+
+    FIRST_NAME_ERROR_LABEL(
+            "First name error label",
+            By.xpath(".//*[@id='firstNameError']")),
+
+    LAST_NAME_ERROR_LABEL(
+            "Last name error label",
+            By.xpath(".//*[@id='lastNameError']")),
+
+    PASSWORD_ERROR_LABEL(
+            "Password error label",
+            By.xpath(".//*[@id='passwordError']")),
+
+    CONFIRM_PASSWORD_ERROR_LABEL(
+            "Confirm password error label",
+            By.xpath(".//*[@id='confirmError']")),
+
+    EMAIL_ERROR_LABEL(
+            "Email error label",
+            By.xpath(".//*[@id='emailError']"));
+
+
     public static final String ROLE_SELECT = ".//label[.='%s']/preceding::input[1]";
-    public static final By CREATE_BUTTON = By.xpath(".//input[@type='submit'][@value='Create']");
-    public static final By LOGIN_NAME_ERROR_LABEL = By.xpath(".//*[@id='nameError']");
-    public static final By FIRST_NAME_ERROR_LABEL = By.xpath(".//*[@id='firstNameError']");
-    public static final By LAST_NAME_ERROR_LABEL = By.xpath(".//*[@id='lastNameError']");
-    public static final By PASSWORD_ERROR_LABEL = By.xpath(".//*[@id='passwordError']");
-    public static final By CONFIRM_PASSWORD_ERROR_LABEL = By.xpath(".//*[@id='confirmError']");
-    public static final By EMAIL_ERROR_LABEL = By.xpath(".//*[@id='emailError']");
+
+    private String name;
+    private By locator;
+
+    NewUserPageLocators(String name, By locator) {
+            this.name = name;
+            this.locator = locator;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public By getBy() {
+        return this.locator;
+    }
 }
