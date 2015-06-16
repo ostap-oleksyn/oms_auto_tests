22a594fed2a75ff8dd3a859d0a3dc951a66417d3
ihor <ihor.ilnytskyi@gmail.com>
2015-06-16 13:30:23 +0300

    Merging.

--- src/test/java/com/softserveinc/edu/ita/dataproviders/DataProviders.java
+++ src/test/java/com/softserveinc/edu/ita/dataproviders/DataProviders.java
@@ -1,43 +1,32 @@
+
 package com.softserveinc.edu.ita.dataproviders;
 
-import com.softserveinc.edu.ita.domains.User;
 import com.softserveinc.edu.ita.dao_jdbc.dao_classes.AbstractDAO;
 import com.softserveinc.edu.ita.dao_jdbc.dao_classes.DAOException;
 import com.softserveinc.edu.ita.dao_jdbc.dao_classes.FactoryDAO;
-import com.softserveinc.edu.ita.enums.UsersTableColumns;
+import com.softserveinc.edu.ita.domains.User;
 import com.softserveinc.edu.ita.enums.Roles;
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
+import com.softserveinc.edu.ita.enums.UsersTableColumns;
+import com.softserveinc.edu.ita.utils.XlsFileReader;
 import org.testng.annotations.DataProvider;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import static com.softserveinc.edu.ita.utils.PropertyLoader.getProperty;
 
 
 public class DataProviders {
-
-    private static HSSFSheet sheet;
-    private static HSSFWorkbook workbook;
 
     @DataProvider(name = "getUsersTableColumns")
     public static Iterator<Object[]> getTestDataIterator() {
         List<Object[]> testDataList = new ArrayList<>();
         try {
             Stream.of(UsersTableColumns.values()).forEach(column -> testDataList.add(new Object[]{column}));
-        }catch(Exception exception){}
+        } catch (Exception exception) {
+        }
         return testDataList.iterator();
     }
 
@@ -48,7 +37,7 @@ public class DataProviders {
     public static Object[][] getAdministratorCredentials() {
         Object[][] credentials = null;
         try {
-            credentials = getUsersByRoleFromXls(Roles.ADMINISTRATOR);
+            credentials = getUsersFromList(Roles.ADMINISTRATOR);
         } catch (IOException | DAOException e) {
             e.printStackTrace();
         }
@@ -62,7 +51,7 @@ public class DataProviders {
     public static Object[][] getCustomerCredentials() {
         Object[][] credentials = null;
         try {
-            credentials = getUsersByRoleFromXls(Roles.CUSTOMER);
+            credentials = getUsersFromList(Roles.CUSTOMER);
         } catch (IOException | DAOException e) {
             e.printStackTrace();
         }
@@ -76,7 +65,7 @@ public class DataProviders {
     public static Object[][] getSupervisorCredentials() {
         Object[][] credentials = null;
         try {
-            credentials = getUsersByRoleFromXls(Roles.SUPERVISOR);
+            credentials = getUsersFromList(Roles.SUPERVISOR);
         } catch (IOException | DAOException e) {
             e.printStackTrace();
         }
@@ -90,7 +79,7 @@ public class DataProviders {
     public static Object[][] getMerchandiserCredentials() {
         Object[][] credentials = null;
         try {
-            credentials = getUsersByRoleFromXls(Roles.MERCHANDISER);
+            credentials = getUsersFromList(Roles.MERCHANDISER);
         } catch (IOException | DAOException e) {
             e.printStackTrace();
         }
@@ -104,7 +93,7 @@ public class DataProviders {
     public static Object[][] getAllRolesUsersFromXLS() {
         Object[][] allRolesUsers = null;
         try {
-            allRolesUsers = getUsersByRoleFromXls(Roles.ALL);
+            allRolesUsers = getUsersFromList(Roles.ALL);
         } catch (DAOException | IOException e) {
             e.printStackTrace();
         }
@@ -118,109 +107,30 @@ public class DataProviders {
     public static Object[][] getInvalidCredentials() {
         Object[][] invalidUsers = null;
         try {
-            invalidUsers = getInvalidUsersFromXls();
+            invalidUsers = XlsFileReader.getAllRowsFromXlsSheet("InvalidCredentials");
         } catch (IOException e) {
             e.printStackTrace();
         }
         return invalidUsers;
     }
 
-    private static Object[][] getUsersByRoleFromXls(Roles roles) throws IOException, DAOException {
-        //TODO move XLS file operations  out to a util class
-        final File excelFile = new File(getProperty("testDataFile"));
-        FileInputStream fileInputStream;
+    private static Object[][] getUsersFromList(Roles role) throws DAOException, IOException {
+        final FactoryDAO factory = new FactoryDAO();
+        final Connection connection = factory.getConnection();
+        final AbstractDAO userDAO = (AbstractDAO) factory.getDAO(connection, User.class);
 
-        try {
-            fileInputStream = new FileInputStream(excelFile);
-            workbook = new HSSFWorkbook(fileInputStream);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        sheet = workbook.getSheet("Users");
-
-        final List<String> listOfUsers = new ArrayList<>();
-
-        Integer columnNumber = null;
-        final String columnName = roles.toString();
-
-        final Row firstRow = sheet.getRow(0);
-
-        for (Cell cell : firstRow) {
-            if (cell.getStringCellValue().equals(columnName)) {
-                columnNumber = cell.getColumnIndex();
-            }
-        }
-
-        if (columnNumber != null) {
-            for (Row row : sheet) {
-                final Cell cell = row.getCell(columnNumber);
-                if (cell == null || cell.getCellType() == Cell.CELL_TYPE_BLANK) {
-                    break;
-                } else {
-                    listOfUsers.add(cell.toString());
-                }
-            }
-        } else {
-            System.out.println("Could not find column " + columnName + " in first row of " + excelFile.toString());
-        }
-
-        FactoryDAO factory = new FactoryDAO();
-        Connection connection = factory.getConnection();
-        AbstractDAO userDAO = (AbstractDAO) factory.getDAO(connection, User.class);
-
-        final List<String> usersLoginFromXls = listOfUsers.stream().skip(1).collect(Collectors.toList());
-
+        final List<String> usersLoginFromXls = XlsFileReader.getColumnFromXlsSheet("Users", role.toString());
         final List<User> users = new ArrayList<>();
         for (String usersLogin : usersLoginFromXls) {
             users.add((User) userDAO.getByLogin(usersLogin));
         }
 
-        Object[][] credibleUserCredentials = new Object[users.size()][1];
+        final Object[][] usersList = new Object[users.size()][1];
 
         for (int i = 0; i < users.size(); i++) {
-            credibleUserCredentials[i][0] = users.get(i);
+            usersList[i][0] = users.get(i);
         }
 
-        return credibleUserCredentials;
-    }
-
-    private static Object[][] getInvalidUsersFromXls() throws IOException {
-        //TODO move XLS file operations  out to a util class
-        final File excelFile = new File(getProperty("testDataFile"));
-        FileInputStream fileInputStream;
-
-        try {
-            fileInputStream = new FileInputStream(excelFile);
-            workbook = new HSSFWorkbook(fileInputStream);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        sheet = workbook.getSheet("InvalidCredentials");
-
-        final int numberOfRows = sheet.getLastRowNum();
-        final int numberOfColumns = sheet.getRow(0).getLastCellNum();
-
-        final String[][] xlsData = new String[numberOfRows][numberOfColumns];
-        String cellValue;
-
-        for (int i = 1; i <= numberOfRows; i++) {
-            final HSSFRow row = sheet.getRow(i);
-
-            for (int j = 0; j < numberOfColumns; j++) {
-                final HSSFCell cell = row.getCell(j);
-                final int cellType = cell.getCellType();
-
-                if (cellType == HSSFCell.CELL_TYPE_FORMULA) {
-                    throw new RuntimeException("Cannot process a formula. Please change field to result of formula.");
-                } else {
-                    cellValue = String.valueOf(cell);
-                    xlsData[i - 1][j] = cellValue;
-                }
-            }
-        }
-        return xlsData;
+        return usersList;
     }
 }
