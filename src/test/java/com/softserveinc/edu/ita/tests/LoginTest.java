0a4c553b237dea28a21066397fab5701ad744207
ostap-oleksyn <ostap.oleksyn@gmail.com>
2015-06-16 11:01:26 +0300

    Resolved ticket IFAA-58

--- src/test/java/com/softserveinc/edu/ita/tests/LoginTest.java
+++ src/test/java/com/softserveinc/edu/ita/tests/LoginTest.java
@@ -1,8 +1,8 @@
 package com.softserveinc.edu.ita.tests;
 
 
-import com.softserveinc.edu.ita.domains.User;
 import com.softserveinc.edu.ita.dataproviders.DataProviders;
+import com.softserveinc.edu.ita.domains.User;
 import com.softserveinc.edu.ita.locators.HomePageLocators;
 import com.softserveinc.edu.ita.locators.UserInfoPageLocators;
 import com.softserveinc.edu.ita.page_object.HomePage;
@@ -11,10 +11,12 @@ import org.testng.annotations.Test;
 
 public class LoginTest extends TestRunner {
 
+    private HomePage homePage;
+    private UserInfoPage userInfoPage;
 
     @Test
     public void emptyLoginTest() {
-        final HomePage homePage = new HomePage(driver);
+        homePage = new HomePage(driver);
         homePage.clickSubmitButton();
         loggingAssert.assertTrue(homePage.isElementDisplayed(HomePageLocators.LOGIN_ERROR_MESSAGE), "Error message displayed");
 
@@ -22,8 +24,8 @@ public class LoginTest extends TestRunner {
 
     @Test(dataProvider = "getAllRoles", dataProviderClass = DataProviders.class)
     public void validLoginTest(User user) {
-        final HomePage homePage = new HomePage(driver);
-        final UserInfoPage userInfoPage = homePage.logIn(user.getLogin(), user.getPassword());
+        homePage = new HomePage(driver);
+        userInfoPage = homePage.logIn(user.getLogin(), user.getPassword());
 
         loggingAssert.assertTrue(userInfoPage
                 .getElementText(UserInfoPageLocators.USER_ROLE_LABEL)
@@ -34,7 +36,7 @@ public class LoginTest extends TestRunner {
 
     @Test(dataProvider = "getInvalidUsers", dataProviderClass = DataProviders.class)
     public void invalidLoginTest(String login, String password) {
-        final HomePage homePage = new HomePage(driver);
+        homePage = new HomePage(driver);
         homePage.logIn(login, password);
 
         loggingAssert.assertTrue(homePage.isElementDisplayed(HomePageLocators.LOGIN_ERROR_MESSAGE), "Error message displayed");
@@ -42,8 +44,8 @@ public class LoginTest extends TestRunner {
 
     @Test(dataProvider = "getAllRoles", dataProviderClass = DataProviders.class)
     public void logOutTest(User user) {
-        final HomePage homePage = new HomePage(driver);
-        final UserInfoPage userInfoPage = homePage.logIn(user.getLogin(), user.getPassword());
+        homePage = new HomePage(driver);
+        userInfoPage = homePage.logIn(user.getLogin(), user.getPassword());
 
         loggingAssert.assertTrue(userInfoPage
                 .getElementText(UserInfoPageLocators.USER_ROLE_LABEL)
---
+++ src/test/java/com/softserveinc/edu/ita/utils/XlsFileReader.java
@@ -0,0 +1,116 @@
+package com.softserveinc.edu.ita.utils;
+
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.softserveinc.edu.ita.utils.PropertyLoader.getProperty;
+
+public final class XlsFileReader {
+
+    private final static String PROPERTY_FILE_NAME = "testDataXlsFile";
+
+    private static HSSFSheet sheet;
+    private static HSSFWorkbook workbook;
+    private static File excelFile;
+    private static FileInputStream fileInputStream;
+
+
+    /**
+     * This method reads all rows from a specific sheet from xls file, skipping the first row with column names.
+     *
+     * @param sheetName - name of the sheet
+     */
+    public static Object[][] readFromXlsByRows(String sheetName) throws IOException {
+        excelFile = new File(getProperty(PROPERTY_FILE_NAME));
+
+        try {
+            fileInputStream = new FileInputStream(excelFile);
+            workbook = new HSSFWorkbook(fileInputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        sheet = workbook.getSheet(sheetName);
+
+        final int numberOfRows = sheet.getLastRowNum();
+        final int numberOfColumns = sheet.getRow(0).getLastCellNum();
+
+        final String[][] xlsData = new String[numberOfRows][numberOfColumns];
+        String cellValue;
+
+        for (int i = 1; i <= numberOfRows; i++) {
+            final HSSFRow row = sheet.getRow(i);
+
+            for (int j = 0; j < numberOfColumns; j++) {
+                final HSSFCell cell = row.getCell(j);
+                final int cellType = cell.getCellType();
+
+                if (cellType == HSSFCell.CELL_TYPE_FORMULA) {
+                    throw new RuntimeException("Cannot process a formula. Please change field to result of formula.");
+                } else {
+                    cellValue = String.valueOf(cell);
+                    xlsData[i - 1][j] = cellValue;
+                }
+            }
+        }
+        return xlsData;
+    }
+
+
+    /**
+     * This method reads a column from specific sheet from xls file, skipping the first row with column names.
+     *
+     * @param sheetName  - name of the sheet
+     * @param columnName - name of the column
+     */
+    public static List<String> readFromXlsByColumns(String sheetName, String columnName) throws IOException {
+        excelFile = new File(getProperty(PROPERTY_FILE_NAME));
+
+        try {
+            fileInputStream = new FileInputStream(excelFile);
+            workbook = new HSSFWorkbook(fileInputStream);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        sheet = workbook.getSheet(sheetName);
+
+        final List<String> xlsData = new ArrayList<>();
+
+        Integer columnNumber = null;
+
+        final Row firstRow = sheet.getRow(0);
+
+        for (Cell cell : firstRow) {
+            if (cell.getStringCellValue().equals(columnName)) {
+                columnNumber = cell.getColumnIndex();
+            }
+        }
+
+        if (columnNumber != null) {
+            for (Row row : sheet) {
+                final Cell cell = row.getCell(columnNumber);
+                if (cell == null || cell.getCellType() == Cell.CELL_TYPE_BLANK) {
+                    break;
+                } else {
+                    xlsData.add(cell.toString());
+                }
+            }
+        }
+
+        return xlsData.stream().skip(1).collect(Collectors.toList());
+    }
+}
