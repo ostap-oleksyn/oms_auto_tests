eeb390331fbe866a984e38a859555718d9f99821
ostap-oleksyn <ostap.oleksyn@gmail.com>
2015-07-13 20:55:49 +0300

    Fixed start-node.sh

--- src/resources/scripts/start-node.sh
+++ src/resources/scripts/start-node.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar /home/osboxes/Grid/selenium-server-standalone-2.45.0.jar -role node -hub http://$1:4444/grid/register -Dwebdriver.chrome.driver=/home/osboxes/Grid/chromedriver -browser browserName=firefox,version=36,maxInstances=5,platform=LINUX -browser browserName=chrome,version=43,maxInstances=5,platform=LINUX -maxSession 5
+java -jar /home/osboxes/Grid/selenium-server-standalone-2.45.0.jar -role node -hub http://$1:4444/grid/register -Dwebdriver.chrome.driver=/home/osboxes/Grid/chromedriver -browser browserName=firefox,version=38,maxInstances=5,platform=LINUX -browser browserName=chrome,version=43,maxInstances=5,platform=LINUX -maxSession 5
